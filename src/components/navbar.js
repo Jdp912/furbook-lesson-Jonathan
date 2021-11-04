11e0df9811dc975d862ea842164313941297aaaf
Jdp912 <jvasquez6461@richmondhillhs.org>
2021-11-04 15:27:25 +0000

    furbook changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ import './css/navbar.css';
 function Navbar() {
   return(
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">Furbook</a>
+      <a className="navbar-cool" href="/">Furbook</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
       </button>
@@ -18,7 +18,7 @@ function Navbar() {
         <a className="nav-link" href="/">Post</a>
         </li>
         <li className="nav-item">
-        <a className="nav-link" href="/">Pet</a>
+        <a className="nav-link" href="https://www.petsmart.com/">Pet</a>
         </li>
       </ul>
       <form className="form-inline my-2 my-lg-0">
